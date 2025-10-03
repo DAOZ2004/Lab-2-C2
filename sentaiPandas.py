5cb788df9fca07a32363d1abe1bb36c1de70e25b
chelesama <daniela01marquez@gmail.com>
2025-10-03 08:26:37 -0600

    complementando

--- sentaiPandas.py
+++ sentaiPandas.py
@@ -13,3 +13,23 @@ print(sentaiDatos['era'].value_counts())
 
 # Promedio de episodios por era
 print(sentaiDatos.groupby('era')['number_of_episode'].mean())
+
+#mostrando los primeros ultimos registros del data set 
+print(sentaiDatos.head())
+
+print(sentaiDatos.tail())
+# Ordenar por rating (de mayor a menor)
+print(sentaiDatos.sort_values(by="rating", ascending=False).head(10))
+
+# Ordenar por número de episodios (de menor a mayor)
+print(sentaiDatos.sort_values(by="number_of_episode", ascending=True).head(10))
+
+#Seleccionar una columna y, calcular al menos dos de las siguientes medidas:
+#a. Media
+#b. Mediana
+#c. Desviación estándar
+# nosotros escogimos hacer las tres 
+print(f"Media: {sentaiDatos['rating'].mean()}")
+print(f"Mediana : {sentaiDatos['rating'].median()}")
+print(f"Varianza : {sentaiDatos['rating'].var()}")
+print(f"Desviacion estandar : {sentaiDatos['rating'].std()}")
