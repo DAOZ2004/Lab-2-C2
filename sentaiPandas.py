ccd0472aebca2138f320e9fdc33b5452170e0cd0
DAOZ <orellanadaniel461@gmail.com>
2025-10-03 08:12:55 -0600

    quitando algunas cosas

--- sentaiPandas.py
+++ sentaiPandas.py
@@ -7,9 +7,3 @@ print(sentaiDatos.describe())
 #identificacion de los tipos de datos
 print(sentaiDatos.dtypes)
 
-
-# Identificar valores únicos en las eras
-print(sentaiDatos['era'].value_counts())
-
-# Promedio de episodios por era
-print(sentaiDatos.groupby('era')['number_of_episode'].mean())
